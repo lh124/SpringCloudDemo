5fb011bd5ab63b96a3b20c5ab1e320bcc1b26357
lh <lh_1215077166@sina.cn>
2017-11-23 22:51:34 +0800

    定义安全配置类的环境搭建

---
+++ MySpringCloud-Security/src/main/java/org/MySpringCloud/Security/App.java
@@ -0,0 +1,13 @@
+package org.MySpringCloud.Security;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
