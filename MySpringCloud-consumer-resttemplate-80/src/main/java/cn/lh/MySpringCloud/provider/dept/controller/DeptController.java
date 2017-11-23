b1a14667271e8294703b1ca2e9e4981bcd88384d
lh <lh_1215077166@sina.cn>
2017-11-23 20:47:22 +0800

    对象的接收采用post请求

--- MySpringCloud-consumer-resttemplate-80/src/main/java/cn/lh/MySpringCloud/provider/dept/controller/DeptController.java
+++ MySpringCloud-consumer-resttemplate-80/src/main/java/cn/lh/MySpringCloud/provider/dept/controller/DeptController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,20 +19,23 @@ public class DeptController {
 	private static final String DEPT_GET_URL="http://dept-8001.com:8001/dept/get";
 	@Resource
 	private RestTemplate restTemplate;
+//	访问地址 http://localhost/consumer/dept/add?dname=12&loc=13
 	@GetMapping("/consumer/dept/add")
-	public Object add(Dept dept) {
+	public Object addRest(Dept dept) {
 		Boolean postForObject = this.restTemplate.postForObject(DEPT_ADD_URL, dept, Boolean.class);
 		return postForObject;
 	}
+//	访问地址 http://localhost/consumer/dept/list
 	@SuppressWarnings("unchecked")
 	@GetMapping("/consumer/dept/list")
-	public Object list() {
+	public Object listRest() {
 		List<Dept> forObject = this.restTemplate.getForObject(DEPT_LIST_URL, List.class);
 		return forObject;
 	}
+//	访问地址 http://localhost/consumer/dept/gets?deptno=1
 	@GetMapping("/consumer/dept/get")
-	public Object get(Long deptno) {
-		Dept forObject = this.restTemplate.getForObject(DEPT_GET_URL, Dept.class);
-		return forObject;
+	public Object getRest(Long deptno) {
+		Dept dept = this.restTemplate.getForObject(DEPT_GET_URL+"/"+deptno, Dept.class);
+		return dept;
 	}
 }
--- MySpringCloud-provider-dept-8001/src/main/java/cn/lh/MySpringCloud/provider/dept/rest/DeptRest.java
+++ MySpringCloud-provider-dept-8001/src/main/java/cn/lh/MySpringCloud/provider/dept/rest/DeptRest.java
@@ -2,6 +2,10 @@ package cn.lh.MySpringCloud.provider.dept.rest;
 
 import javax.annotation.Resource;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,18 +17,18 @@ public class DeptRest {
 	@Resource
 	private IDeptService deptService;
 //	访问地址http://dept-8001.com:8001/dept/list
-	@RequestMapping("/dept/list")
+	@GetMapping("/dept/list")
 	public Object list() {
 		return this.deptService.list();
 	}
 //	访问地址http://dept-8001.com:8001/dept/get?deptno=1
-	@RequestMapping("/dept/get")
-	public Object get(Long deptno) {
+	@GetMapping("/dept/get/{deptno}")
+	public Object get(@PathVariable("deptno") Long deptno) {
 		return this.deptService.get(deptno);
 	}
 //	访问地址http://dept-8001.com:8001/dept/add?dname=11&loc=11
-	@RequestMapping("/dept/add")
-	public Object add(Dept dept) {
+	@PostMapping("/dept/add")
+	public Object add(@RequestBody Dept dept) {
 		return this.deptService.add(dept);
 	}
 }
